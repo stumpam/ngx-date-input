76259adbbecee1b17f66c063ac21f3137fa1005c
Martin Štumpa <stumpa.martin@gmail.com>
2020-03-07 21:22:24 +0100

    fix(lib): remove unnecessary console logs

--- libs/ngx-date-input/src/lib/functions/format.functions.ts
+++ libs/ngx-date-input/src/lib/functions/format.functions.ts
@@ -4,7 +4,7 @@ export const padStart = (
   char: string = '0',
 ) => {
   str = str.toString();
-  console.log(str);
+
   return str.length < length
     ? char + (str.length === 0 && length === 2 ? char : '') + str
     : str;
